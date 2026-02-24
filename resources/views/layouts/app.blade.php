63632d754893fad74c77c191b9c9952851d60173
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-24 21:54:41 +0000

    Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ONCF - Premium Experiences</title>
+    <title>ONCF Booking</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="auth-layout">
