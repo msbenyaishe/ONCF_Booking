1c50b68ff1e5339076290d4f0451351f76355b2b
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-24 22:01:07 +0000

    Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ONCF Booking</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite(['resources/css/style.css', 'resources/js/app.js'])
 </head>
 <body class="auth-layout">
 
