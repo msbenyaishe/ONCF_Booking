3d1a5717eee812f73f5b1c74f0351e230aa3530d
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-25 05:28:47 +0000

    Fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,12 +25,12 @@
 
             <div class="form-group">
                 <label class="form-label">Email Address</label>
-                <input type="email" name="email" class="form-control" placeholder="name@example.com" required>
+                <input type="email" name="email" class="form-control" required>
             </div>
 
             <div class="form-group">
                 <label class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" placeholder="••••••••" required>
+                <input type="password" name="password" class="form-control" required>
             </div>
 
             <button class="btn btn-primary" style="width: 100%; margin-top: 1rem;">
